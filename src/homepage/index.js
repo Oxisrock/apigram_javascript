601726b131c4d0cb7981a0c1a4b5d3023ac4e152
Oxis <franciscoaular18@gmail.com>
2017-09-08 10:33:26 -0400

    probando peticiones con axios

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,23 +3,25 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
-var request = require('superagent');
-var header = require('../header')
+// var request = require('superagent');
+var header = require('../header');
+var axios = require('axios');
 // ruta /signin
-page('/', header, loadPictures, function(ctx, next) {
+page('/', header, loadPicturesAxios, function(ctx, next) {
   title('Apigram');
   var main = document.getElementById('main-container');
 
   empty(main).appendChild(template(ctx.pictures));
 })
 
-function loadPictures (ctx, next) {
-  request
+function loadPicturesAxios (ctx, next) {
+  axios
     .get('/api/pictures')
-    .end(function (err, res) {
-      if (err)
-      { return console.log(err);}
-      ctx.pictures = res.body;
+    .then(function (res) {
+      ctx.pictures = res.data;
       next();
+    })
+    .catch(function (err) {
+      console.log(err);
     })
 }
