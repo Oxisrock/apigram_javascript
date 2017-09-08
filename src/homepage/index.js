f7eb67748b3eae824a4286853b601a7807c997f4
Oxis <franciscoaular18@gmail.com>
2017-09-08 10:40:21 -0400

    probando metodo nativo fecht

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -5,21 +5,34 @@ var template = require('./template');
 var title = require('title');
 // var request = require('superagent');
 var header = require('../header');
-var axios = require('axios');
+// var axios = require('axios');
 // ruta /signin
-page('/', header, loadPicturesAxios, function(ctx, next) {
+page('/', header, loadPicturesFetch, function(ctx, next) {
   title('Apigram');
   var main = document.getElementById('main-container');
 
   empty(main).appendChild(template(ctx.pictures));
 })
 
-function loadPicturesAxios (ctx, next) {
-  axios
-    .get('/api/pictures')
+// function loadPicturesAxios (ctx, next) {
+//   axios
+//     .get('/api/pictures')
+//     .then(function (res) {
+//       ctx.pictures = res.data;
+//       next();
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     })
+// }
+function loadPicturesFetch(ctx, next) {
+  fetch('/api/pictures')
     .then(function (res) {
-      ctx.pictures = res.data;
-      next();
+      return res.json();
+    })
+    .then(function (pictures) {
+      ctx.pictures = pictures;
+            next();
     })
     .catch(function (err) {
       console.log(err);
